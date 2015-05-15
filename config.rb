2718f388102eb68dfdbfb8488807fa5ed4fec631
Nicholas Lue <nlue@mail.nysed.gov>
2015-05-15 11:50:39 -0400

    Upgraded to Foundation 5 and add bower compatibility
    uninstall zurb-foundation gem and install foundation gem

--- config.rb
+++ config.rb
@@ -1,6 +1,4 @@
-# Requre a specific version in this file:
-# gem 'zurb-foundation', '=4.3.1'
-require 'zurb-foundation'
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -11,20 +9,12 @@ images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
 line_comments = true
+color_output = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
